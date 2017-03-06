36c912d92a098648bc1535486e01d14f026242e0
katrinaLHN <haonanl@outlook.com>
2017-03-06 16:57:52 +0800

    2017.3.6 16.57

---
+++ src/tools/requiremain.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lee on 2017/3/4.
+ */
+require.config({
+    baseUrl: "js",
+    paths: {
+        "jquery": ["jquery-3.1.0"],
+        "popup": ["lib/popup"],
+        "moduleA": ["app/moduleA"],
+        "moduleB": ["appmoduleB"]
+    },
+    shim: {
+        'popup': {
+            deps: ['jquery']
+        }
+    }
+});
--- webpack.config.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by lee on 2017/3/3.
- */
